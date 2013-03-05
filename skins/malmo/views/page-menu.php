63e46e3fdaf745cd217232e1eee3efb72ace0714
martent <marten@mbp.local>
2013-03-05 14:40:35 +0100

    Top menu. Added common dir

--- skins/malmo/views/page-menu.php
+++ skins/malmo/views/page-menu.php
@@ -7,7 +7,7 @@
       <?php if ( $wgVectorUseSimpleSearch && $this->getSkin()->getUser()->getOption( 'vector-simplesearch' ) ): ?>
       <div id="simpleSearch" class="input-append">
         <input name="search" title="Sök på Kominwiki [f]" accesskey="f" id="searchInput" type="text" placeholder="Sök wikisida"/>
-        <?php echo $this->makeSearchButton( 'image', array( 'class' => 'btn', 'id' => 'searchButton', 'src' => $this->getSkin()->getSkinStylePath( 'images/search-ltr.png' ), 'width' => '12', 'height' => '13' ) ); ?>
+        <?php echo $this->makeSearchButton( 'image', array( 'class' => 'btn', 'id' => 'searchButton', 'src' => $this->getSkin()->getSkinStylePath( 'images/search-ltr.png' ) ) ); ?>
       <?php else: ?>
       <div>
         <?php echo $this->makeSearchInput( array( 'id' => 'searchInput', 'type' => 'text' ) ); ?>
@@ -19,33 +19,34 @@
     </form>
   </div>
 
+
   <div id="p-views" class="vectorTabs<?php if ( count( $this->data['view_urls'] ) == 0 ) { echo ' emptyPortlet'; } ?>">
     <ul<?php $this->html('userlangattributes') ?>>
+    <li><a href="<?php echo $this->data['namespace_urls']['main']['href'] ?>"><?php echo $this->data['namespace_urls']['main']['text']?></a></li>
       <?php foreach ( $this->data['view_urls'] as $link ): ?>
-        <li<?php echo $link['attributes'] ?>><span><a href="<?php echo htmlspecialchars( $link['href'] ) ?>" <?php echo $link['key'] ?>><?php
+        <?php if( $link['text'] == 'Visa' ) { continue; } ?>
+        <li<?php echo $link['attributes'] ?>><a href="<?php echo htmlspecialchars( $link['href'] ) ?>" <?php echo $link['key'] ?>><?php
           if ( array_key_exists( 'text', $link ) ) {
             echo array_key_exists( 'img', $link ) ?  '<img src="' . $link['img'] . '" alt="' . $link['text'] . '" />' : htmlspecialchars( $link['text'] );
           }
-          ?></a></span></li>
+          ?></a></li>
       <?php endforeach; ?>
 
       <?php if (isset($this->data['content_navigation']['namespaces']['talk'])): ?>
-        <li><span><a href="<?php echo htmlspecialchars( $this->data['content_navigation']['namespaces']['talk']['href'] ) ?>"><?php echo $this->data['content_navigation']['namespaces']['talk']['text'] ?></span></a></li>
+        <li><a href="<?php echo htmlspecialchars( $this->data['content_navigation']['namespaces']['talk']['href'] ) ?>"><?php echo $this->data['content_navigation']['namespaces']['talk']['text'] ?></a></li>
       <?php endif; ?>
       <?php if (isset($this->data['content_navigation']['namespaces']['help_talk'])): ?>
-        <li><span><a href="<?php echo htmlspecialchars( $this->data['content_navigation']['namespaces']['help_talk']['href'] ) ?>"><?php echo $this->data['content_navigation']['namespaces']['help_talk']['text'] ?></span></a></li>
+        <li><a href="<?php echo htmlspecialchars( $this->data['content_navigation']['namespaces']['help_talk']['href'] ) ?>"><?php echo $this->data['content_navigation']['namespaces']['help_talk']['text'] ?></a></li>
       <?php endif; ?>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#" role="button">Åtgärder<span class="icon-caret-down"></span></a>
+        <menu aria-labelledby="actions-menu" class="dropdown-menu" role="menu">
+          <?php foreach ( $this->data['action_urls'] as $link ): ?>
+            <li<?php echo $link['attributes'] ?>><a href="<?php echo htmlspecialchars( $link['href'] ) ?>" <?php echo $link['key'] ?>><?php echo htmlspecialchars( $link['text'] ) ?></a></li>
+          <?php endforeach; ?>
+        </menu>
+      </li>
     </ul>
   </div>
-
-  <div id="p-cactions" class="vectorMenu<?php if ( count( $this->data['action_urls'] ) == 0 ) echo ' emptyPortlet'; ?>">
-    <h5><span><?php $this->msg( 'actions' ) ?></span><a href="#"></a></h5>
-    <div class="menu">
-      <ul<?php $this->html( 'userlangattributes' ) ?>>
-        <?php foreach ( $this->data['action_urls'] as $link ): ?>
-          <li<?php echo $link['attributes'] ?>><a href="<?php echo htmlspecialchars( $link['href'] ) ?>" <?php echo $link['key'] ?>><?php echo htmlspecialchars( $link['text'] ) ?></a></li>
-        <?php endforeach; ?>
-      </ul>
-    </div>
-  </div>
 </div>
+
